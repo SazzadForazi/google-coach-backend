821fcde6d495f0976bc360fe8f27948d589451b5
Sazzad Hossain <sazzad.csebubt@gmail.com>
2022-01-27 11:04:02 +0600

    first commit

--- index.js
+++ index.js
@@ -15,13 +15,21 @@ async function run() {
         const database = client.db("QuestionBank");
         const usersCollection = database.collection("quiz");
         // create a document to insert
-        const doc = {
-            question: "What election year did president Trump lose to Biden?",
-            choices: [2020, 2014],
-            answer: "2020"
-        }
-        const result = await usersCollection.insertOne(doc);
-        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+
+        // const doc = {
+        //     question: "What election year did president Trump lose to Biden?",
+        //     choices: [2020, 2014],
+        //     answer: "2020"
+        // }
+        // const result = await usersCollection.insertOne(doc);
+        // console.log(`A document was inserted with the _id: ${result.insertedId}`);
+
+
+        //Post API
+        app.post('/quiz', async (req, res) => {
+            console.log('hitting the post', req.body);
+            res.send('hit the post');
+        })
     } finally {
         // await client.close();
     }
