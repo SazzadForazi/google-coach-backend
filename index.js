7ba90c2f69e41c6e94dcad7be87207a375792dfd
Sazzad Hossain <sazzad.csebubt@gmail.com>
2022-01-27 11:52:15 +0600

    updated commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const cors = require('cors');
 const app = express();
 const port = 5000;
 
+
+app.use(cors());
+app.use(express.json())
 // user: myQuiz
 // pass: z1yBo8Gt7oLBZZJT
 
